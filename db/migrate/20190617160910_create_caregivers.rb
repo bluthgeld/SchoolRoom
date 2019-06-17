aadc419c12af89bb760228cb1ee625efc570334a
Peter Viss <peter.c.viss@biola.edu>
2019-06-17 12:10:19 -0400

    adding controllers and such

---
+++ db/migrate/20190617160910_create_caregivers.rb
@@ -0,0 +1,13 @@
+class CreateCaregivers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :caregivers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :phone_number
+      t.string :email
+      t.string :contact_preference
+
+      t.timestamps
+    end
+  end
+end
