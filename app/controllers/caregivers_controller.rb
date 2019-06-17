11b72e43aab869f6e91bff2f4d714786795453cc
Peter Viss <peter.c.viss@biola.edu>
2019-06-17 12:28:54 -0400

    crud for controllers

--- app/controllers/caregivers_controller.rb
+++ app/controllers/caregivers_controller.rb
@@ -1,2 +1,45 @@
 class CaregiversController < ApplicationController
+  def index
+    @caregivers = Caregiver.all
+  end
+
+  def show
+    @caregiver = find_caregiver
+  end
+
+  def new
+    @caregiver = Caregiver.new
+  end
+
+  def create
+    @caregiver = Caregiver.create(caregiver_params)
+    @caregiver.save
+    redirect_to @caregiver
+  end
+
+  def edit
+    @caregiver = find_caregiver
+  end
+
+  def update
+    @caregiver = find_caregiver
+    @caregiver.update(caregiver_params)
+    redirect_to caregiver_path(@care_giver)
+  end
+
+  def destroy
+    @caregiver = find_caregiver
+    @caregiver.destroy
+    redirect_to caregivers_path
+  end
+
+  private
+
+  def find_caregiver
+    Caregiver.find_by[params[:id]]
+  end
+
+  def caregiver_params
+    params.require(:caregiver).permit(:first_name, :last_name, :phone_number, :email, :contact_preference)
+  end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,46 @@
 class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = find_student
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+    @student.save
+    redirect_to @student
+  end
+
+  def edit
+    @student = find_student
+  end
+
+  def update
+    @student = find_student
+    @student.update(student_params)
+    redirect_to @student
+  end
+
+  def destroy
+    @student = find_student
+    @student.destroy
+    redirect_to students_path 
+  end
+
+  private
+
+  def find_student
+    Student.find_by(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :age, :start_date)
+  end
+
 end
